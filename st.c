da07558e6e7da0ec7c6ef82b32b9affd8adf227c
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2023-05-19 12:20:46 +0200

    Apply xclearwin patch

--- st.c
+++ st.c
@@ -2011,6 +2011,8 @@ strhandle(void)
 				 * TODO if defaultbg color is changed, borders
 				 * are dirty
 				 */
+                if (j == defaultbg)
+                    xclearwin();
 				tfulldirt();
 			}
 			return;
